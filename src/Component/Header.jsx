a0491ba470bc9e56fbf333fcacc1b7f2d8a1fc81
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-07-17 14:33:40 +0500

    Home Page Completed

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
                     <div className="row align-items-center">
                         <div className="col-md-6 col-12 pb-md-0 pb-5">
                             <p className="section_sub_heading">Hello! world</p>
-                            <p className="section_main_heading">I’m Josh Hartnett <br></br> <span><span>&#10094;</span></span><span className="pink_color">Developer</span><span><span>&#10093;</span></span> </p>
+                            <p className="section_main_heading">I’m Josh Hartnett <br></br> <span><span>&#10094;</span></span><span className="pink_color">Developer</span><span><span>&#10095;</span></span> </p>
                             <p className="section_desc mb-4">Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut dolor sit</p>
                             <button className="btn btn_dark me-3">Hire Me</button>
                             <button className="btn btn_light">Download Resume</button>
